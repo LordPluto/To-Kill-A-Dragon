534fcf2249542f49c51e1a538fdb823f3bd93a49
Shannon Weir <stw3763@rit.edu>
2014-12-27 14:22:59 -0500

    NPC movement
    Changed up how the NPC moves:
    Now there's a prefab called 'NPC Destination' that can be placed on the
    field at will. The object is then added to the NPC's path array, which
    it follows when the boolean npcMovement is true. Paths are looped by
    checking loopPath on the NPC.

--- To Kill a Dragon/Assets/Scripts/Various/NPC/NPCController.cs
+++ To Kill a Dragon/Assets/Scripts/Various/NPC/NPCController.cs
@@ -22,11 +22,18 @@ public class NPCController : MonoBehaviour {
 
 	private GameController _controller;
 
-	private CharacterController _npcControl;
-
-	private Path movementPath;
-	private float stepCount;
 	private bool Talking;
+
+	public bool npcMovement = true;
+
+	public Transform[] pathPoints;
+	private Transform currentPathPoint;
+	private int pointIndex;
+
+	private float moveSpeed = 3;
+	private float pointReached = 1;
+
+	public bool loopPath;
 
 	// Use this for initialization
 	void Start () {
@@ -34,21 +41,17 @@ public class NPCController : MonoBehaviour {
 				_animator = GetComponent<Animator> ();
 				_controller = GameObject.Find ("_GameController").GetComponent<GameController> ();
 
-				_npcControl = GetComponent<CharacterController> ();
-
-				//movementPath = null;
-
-				//This is for testing only. MARKED FOR DELETION
 				_animator.SetTrigger ("Orange");
-				movementPath = new LoopedPath ();
-				movementPath.InsertLineEnd (new Line (180, 5));
-				movementPath.InsertLineEnd (new Line (90, 7));
-				movementPath.InsertLineEnd (new Line (0, 5));
-				movementPath.InsertLineEnd (new Line (270, 7));
-				movementPath.Start ();
 
-				//stepCount = -1;
-				stepCount = movementPath.GetCurrent ().getLength () * 15;
+				if (pathPoints.Length <= 0) {
+						npcMovement = false;
+				}
+
+				if (npcMovement) {
+
+						currentPathPoint = pathPoints [0];
+						pointIndex = 0;
+				}
 
 				Talking = false;
 
@@ -59,52 +62,20 @@ public class NPCController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-				Vector3 change = new Vector3 ();
-				if (movementPath != null && !Talking) {
-						if (stepCount < 0) {
-								if (movementPath.NextLine ()) {
-										stepCount = movementPath.GetCurrent ().getLength () * 15;
-								} else {
-										change = Vector3.zero;
-										_animator.SetFloat ("Speed", 0);
+				if (npcMovement && !Talking) {
+						MoveTowardPoint ();
+		
+						if (Vector3.Distance (currentPathPoint.transform.position, transform.position) < pointReached / 10) {
+								++pointIndex;
+								if (pointIndex > pathPoints.Length - 1) {
+										if (loopPath) {					
+												pointIndex = 0;
+										} else {
+												npcMovement = false;
+										}
 								}
-						} else {
-								switch (movementPath.GetCurrent ().getDirection ()) {
-								case 0:	
-										change = new Vector3 (0, 0, -5);
-										break;
-								case 1:
-										change = new Vector3 (5, 0, 0);
-										break;
-								case 2:
-										change = new Vector3 (0, 0, 5);
-										break;
-								case 3:
-										change = new Vector3 (-5, 0, 0);
-										break;
-								default:
-										Debug.Log ("Somehow an impossible thing has happened.");
-										break;
-								}
-								_animator.SetFloat ("Direction", movementPath.GetCurrent ().getDirection ());
-								_animator.SetFloat ("Speed", 1);
-
-								Ray ray = new Ray (transform.position, change);
-								RaycastHit hit;
-
-								if (Physics.Raycast (ray, out hit, 1) && hit.collider.CompareTag ("Player")) {
-										_animator.SetFloat ("Speed", 0);
-										change = Vector3.zero;
-								}
-								
-						}		
-				} else {
-						change = Vector3.zero;
-						_animator.SetFloat ("Speed", 0);
-				}
-
-				if (change != Vector3.zero && _npcControl.SimpleMove (change)) {
-						stepCount--;
+								currentPathPoint = pathPoints [pointIndex];
+						}
 				}
 		}
 
@@ -134,17 +105,31 @@ public class NPCController : MonoBehaviour {
 		}
 
 	/**
-	 * Sets the NPC's movement path.
-	 * Parameters: Path p - the path to set
-	 * Results: movementPath has been set and started; stepCount is greater than zero.
+	 * Moves the NPC toward the point; if there's something in the way (a player or a wall) it stops.
 	 * **/
-	public void SetPath (Path p) {
-				if (p == null) {
-						return;
+	private void MoveTowardPoint () {
+				Vector3 direction = currentPathPoint.transform.position - transform.position;
+				Vector3 moveVector = direction.normalized * moveSpeed * Time.deltaTime;
+
+				if (direction.z < 0) {
+						_animator.SetFloat ("Direction", 0);
+				} else if (direction.x > 0) {
+						_animator.SetFloat ("Direction", 1);
+				} else if (direction.z > 0) {
+						_animator.SetFloat ("Direction", 2);
+				} else if (direction.x < 0) {
+						_animator.SetFloat ("Direction", 3);
 				}
 
-				movementPath = p;
-				movementPath.Start ();
-				stepCount = movementPath.GetCurrent ().getLength () * 15;
+				Ray ray = new Ray (transform.position, direction);
+				RaycastHit hit;
+
+				if (Physics.Raycast (ray, out hit, moveVector.magnitude) && (hit.collider.CompareTag ("Player") || hit.collider.CompareTag ("Level"))) {
+						_animator.SetFloat ("Speed", 0);
+						return;
+				} else {
+						_animator.SetFloat ("Speed", 1);
+						transform.position += moveVector;
+				}
 		}
 }
