1884a60ee67a5e676d6ab5f7d2ec8c55653d49b7
Shannon Weir <stw3763@rit.edu>
2015-01-19 16:35:20 -0500

    Preliminary Item work
    Added the base item prefab and the preliminary work for collectable
    items
    
    + Item prefab created, basic functionality works (dies on impact to
    Player, tracks him when in a certain range
    
    ~ All new items need to either be made as prefabs in their own right or
    stored in an ItemDrops empty item in the origin.
    
    ! No sort of thing like value for coins or ability to heal HP/MP
    ! Enemies don't drop items on death

---
+++ To Kill a Dragon/Assets/Scripts/Item/ItemMasterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMasterController : MonoBehaviour {
+
+	private ItemAttractMovement attractMode;
+
+	// Use this for initialization
+	void Start () {
+				attractMode = GetComponent<ItemAttractMovement> ();
+		}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		}
+
+	void OnTriggerEnter(Collider c){
+				if (c.CompareTag ("Player")) {
+						Destroy (gameObject);
+				}
+		}
+
+	/**
+	 * Shifts the item's mode to slight magnetism toward the player
+	 * **/
+	public void AttractMode () {
+				attractMode.enabled = true;
+		}
+}
