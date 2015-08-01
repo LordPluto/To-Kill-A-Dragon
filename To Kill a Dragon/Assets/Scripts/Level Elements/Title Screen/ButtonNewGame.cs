33b8485499afd532cce772b87c60423b12054468
Shannon Weir <stw3763@rit.edu>
2015-07-31 20:25:07 -0400

    Title Screen! Also my mind exploded.
    Aaaaaaaaaaaaaahhhhhhhhhhhhhhhhhh
    
    + Aaaaaaaaaahhhhhhhhhhhhh
    + AAAAAAAAAAAAAHHHHHHHHHHHH
    
    ~ Aaaaaaaahhhhhhhhhhhhhh
    
    ! AAAAAhhhhhhhhhhhhh

---
+++ To Kill a Dragon/Assets/Scripts/Level Elements/Title Screen/ButtonNewGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNewGame : MonoBehaviour {
+	public void NewGame(int index){
+		Application.LoadLevel (index);
+	}
+
+	public void NewGame(string levelName){
+		Application.LoadLevel (levelName);
+	}
+}
---
+++ To Kill a Dragon/Assets/Scripts/Level Elements/Title Screen/ButtonQuit.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonQuit : MonoBehaviour {
+	public void Quit() {
+		Application.Quit ();
+	}
+}
