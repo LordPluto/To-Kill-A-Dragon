62ebae98c619c07b2857fb5faed602a31fdc59c2
Shannon Weir <stw3763@rit.edu>
2015-08-09 00:00:01 -0400

    Revert "Advi Image"
    This reverts commit eb32f27b8ba6ffbd991a20e67b46236ed5a0238d.

--- To Kill a Dragon/Assets/Scripts/Various/AdviController.cs
+++ To Kill a Dragon/Assets/Scripts/Various/AdviController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class AdviController : MonoBehaviour {
 
+	public float depth = 2;
+
 	// Use this for initialization
 	void Start () {
 	}
@@ -10,6 +12,7 @@ public class AdviController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Advi's Position
-		transform.position = Input.mousePosition;
+		Vector3 NewPosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, depth));
+		transform.position = NewPosition;
 	}
 }
