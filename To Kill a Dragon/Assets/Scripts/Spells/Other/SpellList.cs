e1bd7735918b1ca2994385842c0165906f129b79
Shannon Weir <stw3763@rit.edu>
2015-12-20 15:04:32 -0500

    Temp commit
    Because Elick screwed something up with the camera, this is a temporary
    commit for fixing it.

--- To Kill a Dragon/Assets/Scripts/Spells/Other/SpellList.cs
+++ To Kill a Dragon/Assets/Scripts/Spells/Other/SpellList.cs
@@ -59,6 +59,10 @@ public class SpellList : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		}
+
+	void Awake () {
+		DontDestroyOnLoad (transform.gameObject);
+	}
 	
 	// Update is called once per frame
 	void Update () {
